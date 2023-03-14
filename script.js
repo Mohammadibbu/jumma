67f7bb61e5aad71386cce4683719bee4facf6e99
Mohammadibbu <92036859+Mohammadibbu@users.noreply.github.com>
2023-03-14 19:22:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
  document.getElementById("submitbtn").addEventListener("click",function(event){event.preventDefault()});
  // validate the form
  function validate() {
+  let jpg=document.getElementById('dwnld');
   let content_table=document.getElementById('content');
   let btn_table=document.getElementById('button');
   let invalid=document.getElementById('check');
@@ -64,7 +65,8 @@
         //alert("✳️👍Login Successfully👍✳️");
         //login success message
         function loginsuccess(){
-    autoclick();
+    autoClick();
+jpg.style.display = "block" ;
           loginbtn.style.color ="black";
           loginbtn.style.background ="yellow";
         content_table.style.display = "block" ;
